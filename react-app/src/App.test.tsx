78f13d043314944f9a5276b86fbc9b5e05047d5c
milky <listingslab@gmail.com>
2022-06-01 21:34:24 +0200

    Adds Material UI

--- react-app/src/App.test.tsx
+++ react-app/src/App.test.tsx
@@ -4,12 +4,12 @@ import { Provider } from 'react-redux';
 import { store } from './app/store';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders skip intro link', () => {
   const { getByText } = render(
     <Provider store={store}>
       <App />
     </Provider>
   );
 
-  expect(getByText(/learn/i)).toBeInTheDocument();
+  expect(getByText(/skip intro/i)).toBeInTheDocument();
 });
--- react-app/src/features/flash/Flash.tsx
+++ react-app/src/features/flash/Flash.tsx
@@ -1,19 +1,33 @@
 import React from 'react'
+
+import {
+ Box,
+ Button,
+} from '@mui/material'
+
 import { SkeletonVariants } from './'
+
 
 export default function Flash() {
 
   return (
-    <div>
+    <Box>
       <h1 style={{fontWeight: "lighter"}}>Skip Intro</h1>
       
       
+      
+      <Button href="https://listingslab.com/flash" target="_blank" rel="noreferrer">
+        flash
+      </Button>
+
+      <Button 
+        variant="contained"
+      href="https://github.com/listingslab/flash" target="_blank" rel="noreferrer">
+        GitHub
+      </Button>
+
       <SkeletonVariants />
 
-      <a href="https://github.com/listingslab/flash" target="_blank" rel="noreferrer">
-        GitHub
-      </a>
-
-    </div>
+    </Box>
   )
 }
