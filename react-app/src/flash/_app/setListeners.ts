899daf73d3154ca1ec455b7367edf526b5a1227e
Wei Zang <weizang75@gmail.com>
2022-06-02 13:06:58 +0200

    listeners

---
+++ react-app/src/flash/_app/setListeners.ts
@@ -0,0 +1,13 @@
+export const setListeners = () => {
+    // console.log ("Settings Listeners...")
+    
+    
+    
+    
+    return ["array of listener slugs"];
+
+
+
+
+
+}
--- react-app/src/flash/index.tsx
+++ react-app/src/flash/index.tsx
@@ -1,41 +1,35 @@
 import { RootState } from './_app/store'
 import Flash from './Flash'
 import Stage from './Stage'
-
+import FlashMenu from './_app/FlashMenu'
+import {setListeners} from './_app/setListeners'
+import Icon from './_app/theme/Icon'
 import {setFlash} from './_app/flashSlice'
-
-
 import PinpongballClip from './MovieClips/PinpongballClip'
 import Mumma from './MovieClips/Mumma'
 import TextClip from './MovieClips/TextClip'
-
-
 import { intro } from './ActionScript/intro'
-
-
-
 import { getDisplay } from './ActionScript/methods/getDisplay'
 import { getElement } from './ActionScript/methods/getElement'
 import { getSizes } from './ActionScript/methods/getSizes'
 import { setPosition } from './ActionScript/methods/setPosition'
-
 import { fade } from './ActionScript/effects/fade'
 
-export const selectFlash = (state: RootState) => state.flash
 export {
-
     Flash,
+    FlashMenu,
     Stage,
     PinpongballClip,
     Mumma,
     TextClip,
-
+    Icon,
     setFlash,
-    
     intro,
     getDisplay,
     getElement,
     setPosition,
     getSizes,
     fade,
+    setListeners,
 }
+export const selectFlash = (state: RootState) => state.flash
